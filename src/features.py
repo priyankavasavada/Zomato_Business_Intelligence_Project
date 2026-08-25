b635f11acb6d31cf6c958d29cc7b7d3a42492b13
Priyanka <Priyanka@Shloks-MacBook-Pro.local>
2026-08-25 20:35:51 +0200

    Performed Data Exploration and wrote profiling functions

---
+++ src/features.py
@@ -0,0 +1,7 @@
+#src/features.py
+def build_features():
+
+    print("Feature engineering script initialized.")
+
+if __name__ == "__main__":
+    build_features()
---
+++ src/model_churn.py
@@ -0,0 +1,6 @@
+#src/model_churn.py
+def train_churn_model():
+    print("Churn model training script initialized.")
+
+if __name__ == "__main__":
+    train_churn_model()
